b2124f8a3b7df411f00416e81cde0063552f4b10
Sage Belknap <sage.belknap@mykenzie.snhu.edu>
2023-07-01 08:29:50 -0600

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Bytes';
+  activeTab:string = 'Personal Details';
+   onTabClick(tab: string){
+     this.activeTab = tab;
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeTabComponent } from './home-tab/home-tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeTabComponent
   ],
   imports: [
     BrowserModule,
