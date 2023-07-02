35463e82d91a3c64cda0163dc0edea2f6f2d45bb
Sage Belknap <sage.belknap@mykenzie.snhu.edu>
2023-07-01 19:22:39 -0600

    Doesn't work the way I want to ( checks should remain there even when the tabs change ) but i fixed the breakage.

--- src/app/monsters-tab/monsters-tab.component.ts
+++ src/app/monsters-tab/monsters-tab.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import monsters from '../monster-box/FakeMonsterData'; // Import the monsters array
+import { CheckedMonstersService } from '../monster-box/CheckedMonstersService';
 
 @Component({
   selector: 'app-monsters-tab',
@@ -7,14 +8,15 @@ import monsters from '../monster-box/FakeMonsterData'; // Import the monsters ar
   styleUrls: ['./monsters-tab.component.scss']
 })
 export class MonstersTabComponent implements OnInit {
-  @Input() checkedMonsters: number[] = [];
   monsterElements: any[] = [];
+
+  constructor(public checkedMonstersService: CheckedMonstersService) {}
 
   ngOnInit() {
     // Use the imported monsters array
     for (let i = 0; i < monsters.length; i++) {
       const monsterBox = {
-        checked: this.checkedMonsters.includes(i),
+        checked: this.checkedMonstersService.checkedMonsters.includes(i),
         ...monsters[i] // Spread the properties from the monsters array
       };
       this.monsterElements.push(monsterBox);
@@ -23,10 +25,17 @@ export class MonstersTabComponent implements OnInit {
 
   onCheckboxChange(index: number, checked: boolean): void {
     this.monsterElements[index].checked = checked;
-    this.checkedMonsters = this.monsterElements
-      .map((monster, idx) => monster.checked ? idx : -1)
-      .filter(idx => idx !== -1);
+
+    if (checked) {
+      // Add the monster to the checkedMonsters array
+      this.checkedMonstersService.checkedMonsters.push(index);
+    } else {
+      // Remove the monster from the checkedMonsters array
+      const indexToRemove = this.checkedMonstersService.checkedMonsters.indexOf(index);
+      if (indexToRemove !== -1) {
+        this.checkedMonstersService.checkedMonsters.splice(indexToRemove, 1);
+      }
+    }
     console.log(`(Monster Tab) Checkbox change detected on index: ${index}`);
   }
-
 }
